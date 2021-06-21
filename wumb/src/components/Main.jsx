1306fca91bfb7f5f07903d4cee6722d0b78f7aea
jataubman <jeremy.taubman@generalassemb.ly>
2021-06-21 10:18:30 -0400

    adding routes and links

--- wumb/src/components/Main.jsx
+++ wumb/src/components/Main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import YTE from './Youtubeembed'
+import Login from '../forms/Login'
+import Register from '../forms/Register'
+
+
+//fetch call here?
+//set embedID in state?
 
 
 function Main(props) {
     return (
         <div>
+
+<Route exact path='/login' render={(props) => <Login {...props} />} />
+<Route exact path='/register' render={(props) => <Register {...props} />} />
        <YTE embedId="G1QjyskJ9jw" />
         </div>
     );
