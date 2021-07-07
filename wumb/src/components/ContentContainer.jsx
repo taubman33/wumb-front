c69fdf0e4f7762ebca5ae16e53b232ec77bc34b3
jataubman <jeremy.taubman@generalassemb.ly>
2021-07-07 14:23:39 -0400

    working again

--- wumb/src/components/ContentContainer.jsx
+++ wumb/src/components/ContentContainer.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import YTE from './Youtubeembed'
-import TableContainer from './TableContainer'
+import Table from './Table'
 import Searchbar from './Searchbar'
 
 
@@ -21,29 +21,29 @@ const EmbedContainer = (embedId, youtubeLink) => {
     const num = 0
 
     
-  //fetch code + return for table.
-  // useEffect(() => {
-  //     fetch(`https://wumb-proxy-2.herokuapp.com/parse?live=true&d=${searchYear}${searchMonth}${searchDay}`)
-  //     .then((res) =>(res.text()))
-  //     .then(body => {
+  // fetch code + return for table.
+  useEffect(() => {
+      fetch(`https://wumb-proxy-2.herokuapp.com/parse?live=true&d=${searchYear}${searchMonth}${searchDay}`)
+      .then((res) =>(res.text()))
+      .then(body => {
 
-  //       const parser = new DOMParser()
-  //       const doc = parser.parseFromString(body, 'text/html')
-  //       const tbs = doc.querySelector("#MainContentTextOnly").querySelectorAll("tbody")
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(body, 'text/html')
+        const tbs = doc.querySelector("#MainContentTextOnly").querySelectorAll("tbody")
 
-  //       const data = Array.from(tbs).map( tb => {
-  //           return {
-  //               time: tb.children[0].children[0].innerText.replaceAll("\n", ""), 
-  //               artist: tb.children[0].children[1].innerText.replaceAll("\n", ""), 
-  //               title: tb.children[1].innerText.replaceAll("\n", "")
-  //           }
-  //       })
-  //       setRadioData(data)
+        const data = Array.from(tbs).map( tb => {
+            return {
+                time: tb.children[0].children[0].innerText.replaceAll("\n", ""), 
+                artist: tb.children[0].children[1].innerText.replaceAll("\n", ""), 
+                title: tb.children[1].innerText.replaceAll("\n", "")
+            }
+        })
+        setRadioData(data)
         
-  //   })
-  //     .catch(console.error);
+    })
+      .catch(console.error);
 
-  // }, []);
+  }, []);
 
 
 
@@ -61,7 +61,7 @@ const EmbedContainer = (embedId, youtubeLink) => {
             </div>
 
             <div className="queue-table">
-            <TableContainer radioData={radioData}
+            <Table radioData={radioData}
                             searchDay={searchDay}
                             searchMonth={searchMonth}
                             searchYear={searchYear}
