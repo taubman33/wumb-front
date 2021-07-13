1863175d3ff6b256d0690adba4ece9574231eec1
rsaleh <raihan.saleh@gmail.com>
2021-07-13 16:19:03 -0400

    removed commented out old code

--- wumb/src/components/Table.jsx
+++ wumb/src/components/Table.jsx
@@ -1,29 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 function Table({ radioData, searchDay, searchMonth, searchYear }) {
-  // const [displayDay, setdisplayDay] = useState("");
-  // const [displayMonth, setdisplayMonth] = useState("");
-  // const [displayYear, setdisplayYear] = useState("");
   const [startRange, setStartRange] = useState(0);
   const [endRange, setEndRange] = useState(10);
   const [displayData, setDisplayData] = useState([]);
-  // const [num0, setNum0] = useState(0);
-  // const [num1, setNum1] = useState(1);
-  // const [num2, setNum2] = useState(2);
-  // const [num3, setNum3] = useState(3);
-  // const [num4, setNum4] = useState(4);
-  // const [num5, setNum5] = useState(5);
-  // const [num6, setNum6] = useState(6);
-  // const [num7, setNum7] = useState(7);
-  // const [num8, setNum8] = useState(8);
-  // const [num9, setNum9] = useState(9);
-
-  // useEffect(() => {
-  //   setdisplayDay(searchDay);
-  //   setdisplayMonth(searchMonth);
-  //   setdisplayYear(searchYear);
-  //   setdisplayData(radioData);
-  // }, [searchYear, searchMonth, searchDay, radioData]);
 
   useEffect(() => {
     const radioDataToDisplay = [];
@@ -36,19 +16,6 @@ function Table({ radioData, searchDay, searchMonth, searchYear }) {
 
   const nextBatch = () => {
     // needs an if statement to avoid an error at the end of the list
-
-    // console.log("first", num0, num1, num2);
-    // setNum0(num0 + 10);
-    // setNum1(num1 + 10);
-    // setNum2(num2 + 10);
-    // setNum3(num3 + 10);
-    // setNum4(num4 + 10);
-    // setNum5(num5 + 10);
-    // setNum6(num6 + 10);
-    // setNum7(num7 + 10);
-    // setNum8(num8 + 10);
-    // setNum9(num9 + 10);
-    // console.log("second", num0, num1, num2);
     setStartRange(startRange + 10);
     setEndRange(endRange + 10);
   };
@@ -57,18 +24,6 @@ function Table({ radioData, searchDay, searchMonth, searchYear }) {
     if (startRange < 10) {
       alert("Too far, go to next day!");
     } else {
-      // console.log("first", num0, num1, num2);
-      // setNum0(num0 - 10);
-      // setNum1(num1 - 10);
-      // setNum2(num2 - 10);
-      // setNum3(num3 - 10);
-      // setNum4(num4 - 10);
-      // setNum5(num5 - 10);
-      // setNum6(num6 - 10);
-      // setNum7(num7 - 10);
-      // setNum8(num8 - 10);
-      // setNum9(num9 - 10);
-      // console.log("second", num0, num1, num2);
       setStartRange(startRange - 10);
       setEndRange(endRange - 10);
     }
